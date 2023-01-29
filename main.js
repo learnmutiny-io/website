4fd196871c0fb09379be13ff5dba039ab9b8ac40
Zion Melson <zionmelson@gmail.com>
2023-01-28 21:12:54 -0500

    added sign-up

--- main.js
+++ main.js
@@ -1,34 +1,71 @@
-//import { storing, getting } from "./db";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5A2GeyIgaWaapl0adfp77wph7q3w6ds0",
+  authDomain: "learnmutiny-60952.firebaseapp.com",
+  projectId: "learnmutiny-60952",
+  storageBucket: "learnmutiny-60952.appspot.com",
+  messagingSenderId: "554213932048",
+  appId: "1:554213932048:web:17ad647356f22fe261de95",
+};
+
+const firebase = initializeApp(firebaseConfig);
 
 const app = (document.querySelector("#app").innerHTML = `
   <div>
-    <img src="./assets/trio.png" class="background" />
     <div class="main">
-      <video class="video" width="320" height="240" autoplay>
-      </video>
+    <img src="./assets/bear.PNG" alt="bear2" />
       <h1 class="title">learnmutiny.</h1>
-      <button id="next">learn</button>
+        <form id="form">
+          <input class="inputs" type="text" id="username" placeholder="username." />
+          <input class="inputs" type="email" id="email" placeholder="email." />
+          <button id="next">learn.</button>
+        </form>
     <div>
   </div>
 `);
 
 const nextButton = document.querySelector("#next");
-const video = document.querySelector(".video");
-
-let played = true;
 
 nextButton.addEventListener("click", (e) => {
   e.preventDefault();
-  if (played) {
-    showVideo();
 
-    played = false;
-    nextButton.className = "#next-clicked";
+  const email = document.getElementById("email").value;
+  const username = document.getElementById("username").value;
+
+  let usernameVal = validateUsername(username);
+  if (usernameVal == false) {
+    return;
   }
+  let emailVal = validateEmail(email);
+  if (emailVal == false) {
+    return;
+  }
+
+  storeEmail(emailVal, usernameVal);
 });
 
-function showVideo() {
-  video.setAttribute("src", "./assets/learn.mp4");
-  video.load();
-  video.play();
+function storeEmail(email, name) {
+  const db = getDatabase();
+  set(ref(db, `users/${name}`), {
+    username: name,
+    email: email,
+  });
+}
+
+function validateEmail(email) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    return email;
+  }
+  alert("You have entered an invalid email address");
+  return false;
+}
+
+function validateUsername(username) {
+  if (username.includes(".")) {
+    alert("Invalid username, no '.'");
+  } else {
+    return username;
+  }
 }
