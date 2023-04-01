9bfe3f41cfb7f4f113ec7861ce9c87f8226fe579
zionmelson <92884383+zionmelson@users.noreply.github.com>
2023-04-01 09:45:48 -0400

    checking cors

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,5 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const cors = require("cors")({ origin: true });
 const sgMail = require("@sendgrid/mail");
 
 admin.initializeApp(functions.config().firebase);
@@ -11,33 +10,31 @@ sgMail.setApiKey(SENDGRID_API);
 
 // Cloud Function to send email from form
 exports.sendEmailFromForm = functions.https.onRequest(async (req, res) => {
-  cors()(req, res, async () => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.set("Access-Control-Allow-Origin", "https://learnmutiny.io");
+  res.set("Access-Control-Allow-Methods", "GET, POST");
+  res.set("Access-Control-Allow-Headers", "Content-Type");
 
-    if (req.method === "POST") {
-      const { name, email, userType } = req.body;
-      const msg = {
-        to: email,
-        from: "admin@learnmutiny.io",
-        templateId: SENDGRID_TEMPLATE,
-        dynamic_template_data: {
-          name: name,
-          userType: userType,
-        },
-        subject: "welcome to learnmutiny",
-      };
-      try {
-        await sgMail.send(msg);
-        console.log("Email sent successfully");
-        res.status(200).send("Email sent successfully");
-      } catch (error) {
-        console.error(error);
-        res.status(500).send("Error sending email");
-      }
-    } else {
-      res.status(405).send("Method Not Allowed");
+  if (req.method === "POST") {
+    const { name, email, userType } = req.body;
+    const msg = {
+      to: email,
+      from: "learnmutiny@gmail.com",
+      templateId: SENDGRID_TEMPLATE,
+      dynamic_template_data: {
+        name: name,
+        userType: userType,
+      },
+      subject: "welcome to learnmutiny",
+    };
+    try {
+      await sgMail.send(msg);
+      console.log("Email sent successfully");
+      res.status(200).send("Email sent successfully");
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error sending email");
     }
-  });
+  } else {
+    res.status(405).send("Method Not Allowed");
+  }
 });
