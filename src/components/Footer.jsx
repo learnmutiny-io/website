f75ada53968398a577b0d4328bf1a5e868c194a3
SaiRamyaKamali <srkamali1326@gmail.com>
2023-06-02 21:14:54 -0500

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,12 +11,17 @@ const Footer = () => {
         <div className="w-full flex py-6 justify-between items-center sm:mt-0 mt-20">
             <div className=" flex-col justify-end sm:items-end items-center flex-1 flex">
                 <div className="justify-end items-center flex-1 flex">
-                    <div className="bg-dimPrimary w-12 h-12 flex items-center justify-center rounded-full">
-                        <FontAwesomeIcon icon={faLinkedin} className="text-white text-2xl" />
-                    </div>
-                    <div className="bg-dimPrimary w-12 h-12 flex items-center justify-center rounded-full">
-                        <FontAwesomeIcon icon={faInstagram} className="text-white text-2xl" />
-                    </div>
+                    <a href="https://www.linkedin.com/company/learnmutiny/" target='_blank'>
+                        <div className="bg-dimPrimary w-12 h-12 flex items-center justify-center rounded-full">
+                            <FontAwesomeIcon icon={faLinkedin} className="text-white text-2xl" />
+                        </div>
+                    </a>
+                    <a href="https://www.instagram.com/learnmutiny/" target='_blank'>
+                        <div className="bg-dimPrimary w-12 h-12 flex items-center justify-center rounded-full">
+                            <FontAwesomeIcon icon={faInstagram} className="text-white text-2xl" />
+                        </div>
+                    </a>
+
                 </div>
                 <div className="justify-end items-center flex-1 flex">
                     <p className={`${styles.paragraph} font-[800] mr-2 mt-8`}>learnmutiny corporation</p>
